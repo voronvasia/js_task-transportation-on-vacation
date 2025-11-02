ad5dbe944ea7e20b222cca083718f8122338456a
Vasyl Vorontsov <voronvasia@gmail.com>
2025-11-02 20:16:52 +0200

    task completed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const priceForDay = 40;
+  const discountFor3Days = 20;
+  const discountFor7Days = 50;
+
+  let totalCost = days * priceForDay;
+
+  if (days >= 7) {
+    totalCost -= discountFor7Days;
+  } else if (days >= 3) {
+    totalCost -= discountFor3Days;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
